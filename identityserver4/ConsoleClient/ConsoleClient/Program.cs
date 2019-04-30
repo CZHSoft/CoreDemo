f29b6f69f38c907d2e75d28c3b031cd856b559e5
CZHSoft <chenandczh@163.com>
2019-04-30 09:43:05 +0800

    add console client

---
+++ identityserver4/ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // discovery endpoint
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000/");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request access token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "console client",
+                ClientSecret = "console secrect",
+                Scope = "CZHSoftApi"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            
+            // call Identity Resource API
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = await apiClient.GetAsync("http://localhost:5001/api/values");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
